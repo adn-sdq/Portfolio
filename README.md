a077abd83e0e5a385e0fd547f3f208aa874b3930
adn-sdq <adn.memos@gmail.com>
2025-02-12 01:04:34 +0530

    Add initial project setup with Next.js, Tailwind CSS, and ESLint configuration

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Portfolio
+
 Adnan Memos Portfolio
---
+++ portfolio/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <div className="text-sm text-center">
+        Sometimes I wish I was the Goodbye.
+      </div>
+    </div>
+  );
+}
